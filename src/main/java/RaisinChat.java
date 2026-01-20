2a3c1ea61494524fa5a77b30bfb3d9b6984f8621
RaisinMoldyBread <dylankoh02@gmail.com>
2026-01-20 22:36:57 +0800

    Add main logic for task 1
    Main task runs in a while loop, when we receive a bye string, we exit the loop to exit the programme

--- src/main/java/RaisinChat.java
+++ src/main/java/RaisinChat.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RaisinChat {
     static String chatName = "RaisinChat";
     static String logo = "__________        .__       .__       _________ .__            __   \n" +
@@ -9,12 +11,32 @@ public class RaisinChat {
     static String borders = "----------------------------------------------";
     public static void main(String[] args) {
         System.out.println(logo);
+
         System.out.println(borders);
         System.out.println("Hello! I'm " + chatName);
         System.out.println("What can I do for you?");
         System.out.println(borders);
-        System.out.println("Bye. Hope ot see you again soon!");
+        boolean waitUser = true;
+        Scanner scanner  = new Scanner(System.in);
+        while (waitUser) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                waitUser = false;
+            }
+            else if (userInput.equalsIgnoreCase("help")) {
+                printOutput("List - List all available tasks \n" +
+                        "Help - List all commands available to chatbot \n" +
+                        "Bye - Exit Chatbot :(");
+            }
+            else {
+                printOutput(userInput);
+            }
+
+        }
         System.out.println(borders);
+        System.out.println("Bye :\") Please come back again :\")");
+        System.out.println(borders);
+
     }
 
     public static void printOutput(String data) {

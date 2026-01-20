f7c2b2e8a50c927f041bf3529fcbf0c042975683
RaisinMoldyBread <dylankoh02@gmail.com>
2026-01-20 23:04:25 +0800

    Add logic for Task listing

--- src/main/java/RaisinChat.java
+++ src/main/java/RaisinChat.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RaisinChat {
@@ -9,15 +11,16 @@ public class RaisinChat {
             " |____|_  /(____  /__/____  >__|___|  /\\______  /___|  (____  /__|  \n" +
             "        \\/      \\/        \\/        \\/        \\/     \\/     \\/      ";
     static String borders = "----------------------------------------------";
+    static List<Task> listOfTask = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println(logo);
-
         System.out.println(borders);
         System.out.println("Hello! I'm " + chatName);
         System.out.println("What can I do for you?");
         System.out.println(borders);
         boolean waitUser = true;
         Scanner scanner  = new Scanner(System.in);
+
         while (waitUser) {
             String userInput = scanner.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
@@ -27,6 +30,16 @@ public class RaisinChat {
                 printOutput("List - List all available tasks \n" +
                         "Help - List all commands available to chatbot \n" +
                         "Bye - Exit Chatbot :(");
+            }
+            else if (userInput.equalsIgnoreCase("list")) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < listOfTask.size(); i++) {
+                    sb.append(i)
+                            .append(". ")
+                            .append(listOfTask.get(i).toString())
+                            .append("\n");
+                }
+                printOutput(sb.toString());
             }
             else {
                 printOutput(userInput);

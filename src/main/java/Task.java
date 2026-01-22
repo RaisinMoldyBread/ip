c72941426bdba48ad698442c8b492d3fce79fd23
Dylan Koh <dylankoh02@gmail.com>
2026-01-22 12:32:36 +0800

    Add task marking methods in Task.java
    I also made the name of task and isDone boolean as protected, only public methods are allowed to change the isDone variable

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,37 @@
 public class Task {
-    String taskName;
+    protected String taskName;
+    protected Boolean isDone;
 
     public Task(String initName) {
         this.taskName = initName;
+        this.isDone = false;
+    }
+
+    public String markDone() {
+        if (this.isDone) {
+            return "This task was already marked as done in the past!";
+        }
+        else {
+            this.isDone = true;
+            return String.format("Great job completing the task!\n" +
+                    "I've gone ahead and mark it as done!\n%s", this.toString());
+        }
+    }
+
+    public String unMark() {
+        if (!this.isDone) {
+            return "Oop... you  actually haven't completed this task!";
+        }
+        else {
+            this.isDone = false;
+            return String.format("Hmm... falling behind are we?\n" +
+                    "I've unmarked the task as done!\n%s", this.toString());
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("%s", this.taskName);
+        return this.isDone ? String.format("[X] %s", this.taskName)
+                : String.format("[ ] %s", this.taskName);
     }
 }

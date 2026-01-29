319f3f3cb8bc5fff8a0d9a1adc0c3220c80df561
RaisinMoldyBread <dylankoh02@gmail.com>
2026-01-29 12:39:18 +0800

    Update methods to do real datetime parse

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String startBy;
-    protected String endBy;
+    protected LocalDateTime startBy;
+    protected LocalDateTime endBy;
 
-    public Event(String name, boolean haveDone, String start, String end) {
+    public Event(String name, boolean haveDone, LocalDateTime start, LocalDateTime end) {
         super(name, haveDone);
         this.startBy = start;
         this.endBy = end;
     }
 
+    public String fullString() {
+        return String.format("E | %s | %s -> %s",
+                super.toString(),
+                this.startBy,
+                this.endBy);
+    }
+
     @Override
     public String toString() {
-        return String.format("E | %s | %s - %s",
-                super.toString(), this.startBy, this.endBy);
+        return String.format("E | %s | %s -> %s",
+                super.toString(),
+                this.startBy.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")),
+                this.endBy.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")));
     }
 }

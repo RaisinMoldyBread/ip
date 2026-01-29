2eb5a78cc9912ac73ff0f7d66641d483d9f70410
RaisinMoldyBread <dylankoh02@gmail.com>
2026-01-29 15:09:07 +0800

    Update Ui class with more commands

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ui {
+
+    private static final String CHATNAME = "RaisinChat";
+    private static final String LINE = "___________________________________________";
+    private static final String LOGO = "__________        .__       .__       _________ .__            __   \n"
+            + "\\______   \\_____  |__| _____|__| ____ \\_   ___ \\|  |__ _____ _/  |_ \n"
+            + " |       _/\\__  \\ |  |/  ___/  |/    \\/    \\  \\/|  |  \\\\__  \\\\   __\\\n"
+            + " |    |   \\ / __ \\|  |\\___ \\|  |   |  \\     \\___|   Y  \\/ __ \\|  |  \n"
+            + " |____|_  /(____  /__/____  >__|___|  /\\______  /___|  (____  /__|  \n"
+            + "        \\/      \\/        \\/        \\/        \\/     \\/     \\/      ";
+    private final Scanner scanner;
+
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    public void showLoadingError() {
+        System.out.println("Oh no... we are unable to read your past data :\")");
+    }
+
+    public void showWelcome() {
+        System.out.println(LOGO);
+        String intro = String.format("Hello! I'm %s\n" +
+                "What can I do for you today?", CHATNAME);
+        showMessage(intro);
+    }
+
+    public void showGoodbye() {
+        showMessage("Bye :\") Please come back again :\")");
+    }
+
 }

4fbf6ecb2373740472ea9bc397f6a8ebb3c410e9
RaisinMoldyBread <dylankoh02@gmail.com>
2026-02-04 23:00:02 +0800

    Fix test methods for DeadlineCommandTest

--- src/test/java/raisinchat/command/DeadlineCommandTest.java
+++ src/test/java/raisinchat/command/DeadlineCommandTest.java
@@ -2,7 +2,9 @@ package raisinchat.command;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import raisinchat.Parser;
@@ -13,26 +15,37 @@ import raisinchat.ui.Ui;
 
 public class DeadlineCommandTest {
 
+
+    private Ui ui;
+    private TaskList tasks;
+    private Storage storage;
+
+    /**
+     * Initialises tests' common assets
+     *
+     */
+    @BeforeEach
+    public void setUp() {
+        ui = new Ui();
+        tasks = new TaskList(null);
+        storage = new Storage("./data/RaisinChatTaskTestDb.txt");
+    }
+
     /**
      * Test for standard deadline command if inputs are properly parsed and added to task list
      *
      */
     @Test
-    public void deadlineCommandTest1() {
-        String simulatedInput = "deadline finish stats /by 2026-01-03 11:00 PM";
-        Ui ui = new Ui();
-        TaskList tasks = new TaskList(null);
-        Storage storage = new Storage("./data/RaisinChatTaskTestDb.txt");
-        try {
-            Command c = Parser.parse(simulatedInput);
-            c.execute(tasks, ui, storage);
-            assertEquals("D | 0 | finish stats | Jan 3 2026 11:00 pm", tasks.getTasks(0).toString());
-        } catch (RaisinChatException e) {
-            assertEquals("Hmm, you are doing it wrong! Use command like this: "
-                            + "deadline <taskName> /by <yyyy-MM-dd hh:mm AM/PM>",
-                    e.getMessage());
-        }
+    public void deadline_validInput_taskAddedCorrectly() throws RaisinChatException {
+        Command c = Parser.parse("deadline finish stats /by 2026-01-03 11:00 PM");
 
+        c.execute(tasks, ui, storage);
+
+        assertEquals(1, tasks.size());
+        assertEquals(
+                "D | 0 | finish stats | Jan 3 2026 11:00 pm",
+                tasks.getTasks(0).toString()
+        );
     }
 
     /**
@@ -40,21 +53,18 @@ public class DeadlineCommandTest {
      *
      */
     @Test
-    public void deadlineCommandTest2() {
-        String simulatedInput = "deadline /by 2026-01-03 11:00 PM";
-        Ui ui = new Ui();
-        TaskList tasks = new TaskList(null);
-        Storage storage = new Storage("./data/RaisinChatTaskTestDb.txt");
-        try {
-            Command c = Parser.parse(simulatedInput);
-            c.execute(tasks, ui, storage);
-            assertEquals("D | 0 | finish stats | Jan 3 2026 11:00 pm", tasks.getTasks(0).toString());
-        } catch (RaisinChatException e) {
-            assertEquals("Hmm, you are doing it wrong! Use command like this: "
-                            + "deadline <taskName> /by <yyyy-MM-dd hh:mm AM/PM>",
-                    e.getMessage());
-        }
+    public void deadline_missingTaskName_throwsException() throws RaisinChatException {
+        Command c = Parser.parse("deadline /by 2026-01-03 11:00 PM");
 
+        RaisinChatException e = assertThrows(
+                RaisinChatException.class, () -> c.execute(tasks, ui, storage)
+        );
+
+        assertEquals(
+                "Hmm, you are doing it wrong! Use command like this: "
+                        + "deadline <taskName> /by <yyyy-MM-dd hh:mm AM/PM>",
+                e.getMessage()
+        );
     }
 
     /**
@@ -62,21 +72,18 @@ public class DeadlineCommandTest {
      *
      */
     @Test
-    public void deadlineCommandTest3() {
-        String simulatedInput = "deadline finish stats /by 11:00 PM";
-        Ui ui = new Ui();
-        TaskList tasks = new TaskList(null);
-        Storage storage = new Storage("./data/RaisinChatTaskTestDb.txt");
-        try {
-            Command c = Parser.parse(simulatedInput);
-            c.execute(tasks, ui, storage);
-            assertEquals("D | 0 | finish stats | 11:00 pm", tasks.getTasks(0).toString());
-        } catch (RaisinChatException e) {
-            assertEquals("Hmm, you are doing it wrong! Use command like this: "
-                            + "deadline <taskName> /by <yyyy-MM-dd hh:mm AM/PM>",
-                    e.getMessage());
-        }
+    public void deadline_incompleteDateTime_throwsException() throws RaisinChatException {
+        Command c = Parser.parse("deadline finish stats /by 11:00 PM");
 
+        RaisinChatException e = assertThrows(
+                RaisinChatException.class, () -> c.execute(tasks, ui, storage)
+        );
+
+        assertEquals(
+                "Hmm, you are doing it wrong! Use command like this: "
+                        + "deadline <taskName> /by <yyyy-MM-dd hh:mm AM/PM>",
+                e.getMessage()
+        );
     }
 
     /**
@@ -84,12 +91,9 @@ public class DeadlineCommandTest {
      *
      */
     @Test
-    public void deadlineCommandTest4() {
-        try {
-            Command c = Parser.parse("deadline finish stats /by 11:00 PM");
-            assertFalse(c.isExit());
-        } catch (RaisinChatException e) {
-            assertEquals("Empty command received! Type 'help' to see more commands!", e.getMessage());
-        }
+    public void deadlineCommand_isNotExitCommand() throws RaisinChatException {
+        Command c = Parser.parse("deadline finish stats /by 2026-01-03 11:00 PM");
+
+        assertFalse(c.isExit());
     }
 }
